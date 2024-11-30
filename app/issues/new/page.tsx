8bd4df79428c801dfa0e64a9c2e7bcbac93475a7
yahya bnoun <yahyaiko22@gmail.com>
2024-11-30 11:20:15 +0100

    Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import { useForm, Controller } from 'react-hook-form';
 import dynamic from 'next/dynamic';
 import "easymde/dist/easymde.min.css";
@@ -23,24 +23,39 @@ const NewIssuesPage = () => {
         { resolver: zodResolver(createIssueSchema) }
     );
 
-    return (
-        <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
-            try {
-                setIsSubmitting(true)
-                await axios.post('/api/issues', data)
-                router.push('/issues')
-            } catch (error) {
-                setIsSubmitting(false)
-                setErrorServer("An error occurred, please try again later")
-            }
+
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setIsSubmitting(true)
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+        } catch (error) {
+            setIsSubmitting(false)
+            setErrorServer("An error occurred, please try again later")
         }
-        )}>
+    }
+    )
+
+    return (<>
+    <div className='max-w-xl'>
+    {errorserver &&
+            <Callout.Root color='red' className='mb-4'>
+                <Callout.Text>
+                    {errorserver}
+                </Callout.Text>
+            </Callout.Root>}
+
+        <form className='space-y-3' onSubmit={onSubmit}>
             <TextField.Root placeholder="Title" {...register('title')} />
             <ErrorMessage>{errors.title?.message}</ErrorMessage>
             <Controller name='description' control={control} render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
             <ErrorMessage>{errors.description?.message}</ErrorMessage>
             <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />} </Button>
         </form>
+    </div>
+
+    </>
+
     );
 }
 
